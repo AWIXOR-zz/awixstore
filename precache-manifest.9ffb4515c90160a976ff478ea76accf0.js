ba0b9c41a3d698f0772d1127688471eb7c37a249
H.AOUASSAR <hssine36@gmail.com>
2019-12-28 22:05:11 +0100

    Updates

---
+++ precache-manifest.9ffb4515c90160a976ff478ea76accf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6837d4778fec7c3041369c770684b7",
+    "url": "/awixstore/index.html"
+  },
+  {
+    "revision": "243337f8dd7b47a01f13",
+    "url": "/awixstore/static/css/main.5a262fee.chunk.css"
+  },
+  {
+    "revision": "221c2b78bcd4912084db",
+    "url": "/awixstore/static/js/2.923693cb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/awixstore/static/js/2.923693cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "243337f8dd7b47a01f13",
+    "url": "/awixstore/static/js/main.e9ef5e2e.chunk.js"
+  },
+  {
+    "revision": "a9ad44d39fe859f98e64",
+    "url": "/awixstore/static/js/runtime-main.2977a7a2.js"
+  },
+  {
+    "revision": "c4923c840b5c83f26555a43ef95ddb3e",
+    "url": "/awixstore/static/media/bg.c4923c84.png"
+  },
+  {
+    "revision": "48ed45d5d158775ce4e962d631b69d2e",
+    "url": "/awixstore/static/media/logo.48ed45d5.svg"
+  }
+]);
