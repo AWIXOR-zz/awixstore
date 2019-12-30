ad1f5c3f4dc00c2337f4d9381ce3557540b787c2
H.AOUASSAR <hssine36@gmail.com>
2019-12-30 01:51:53 +0100

    fixing the sign in with google

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Awixstore
 
-Awixstore E-commerce website: [Awixstore](https://AWIXOR.github.io/awixstore/) for more information.
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/ed5d5eac-ae4a-4874-8b54-3d6bed204445/deploy-status)](https://app.netlify.com/sites/awixstore/deploys)
+
+### Awixstore E-commerce website
+
+For more information: [Awixstore](https://awixstore.netlify.com).
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,8 +3,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-console.log(process.env);
-
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +10,8 @@ const config = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_App_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_Measurement_Id
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
