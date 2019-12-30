a2a64a69cf49e9e46f53287ef4ec8034d9c8e005
H.AOUASSAR <hssine36@gmail.com>
2019-12-30 01:22:04 +0100

    Hidding API keys

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,17 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+console.log(process.env);
+
 const config = {
-  apiKey: "AIzaSyD79qwiQ3R7nvThin2wnzORpFAJJQnqE0U",
-  authDomain: "awixstore-c11bf.firebaseapp.com",
-  databaseURL: "https://awixstore-c11bf.firebaseio.com",
-  projectId: "awixstore-c11bf",
-  storageBucket: "awixstore-c11bf.appspot.com",
-  messagingSenderId: "136797185468",
-  appId: "1:136797185468:web:d2babc0c3a2455de40b7b9",
-  measurementId: "G-48XYC89NJY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_App_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_Measurement_Id
 };
 
 firebase.initializeApp(config);
